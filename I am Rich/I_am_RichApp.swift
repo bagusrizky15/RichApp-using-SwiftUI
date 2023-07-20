3229296b17bd91efd43ece4e4c677dc29dec8e57
bagusrizky15 <mohamad.15@students.amikom.ac.id>
2023-07-20 08:29:24 +0700

    Initial Commit

---
+++ I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by bagus on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
